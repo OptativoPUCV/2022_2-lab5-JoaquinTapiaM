7765f496510a9751ea5962aff19effbb9de6695e
Replit user <>
2022-09-24 15:44:32 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -212,12 +212,16 @@ Pair * upperBound(TreeMap * tree, void* key) {
     return NULL;
 }
 
+//6.- Implemente las funciones para recorrer la estructura: Pair* firstTreeMap(TreeMap* tree) retorna el primer **Pair** del mapa (el menor). Pair* nextTreeMap(TreeMap* tree)  retornar el siguiente **Pair** del mapa a partir del puntero TreeNode* current. Recuerde actualizar este puntero.
+
 Pair * firstTreeMap(TreeMap * tree) {
   if(tree->root == NULL){
     return NULL;
   }
-  Pair * par = tree->root->pair;
-  return par;
+  TreeNode * aux = tree->root;
+  while(aux->left != NULL){
+    aux = aux->left;
+  }
 }
 
 Pair * nextTreeMap(TreeMap * tree) {
